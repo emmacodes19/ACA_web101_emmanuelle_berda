a6cc301920d7e8032e320ed5e5de4067a588fae4
emmacodes19 <berdae@gmail.com>
2019-03-06 18:29:08 -0600

    news

---
+++ Dashboard/geolocation.js
@@ -0,0 +1,38 @@
+
+
+
+// var loc = document.getElementById('myloc');
+
+
+    var loc = document.getElementById('myloc');
+    console.log(loc)
+    function myLocation() {
+        console.log("location:", loc)
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            loc.innerHTML = "Location Tracking not Possible.";
+        }
+    }
+
+    function showPosition(position) {
+        console.log(position.coords.longitude)
+        loc.innerHTML = "longitude:" + position.coords.longitude + " <br> Latitude:" + position.coords.latitude + "<br>";
+    }
+
+
+// function myLocation() {
+//     console.log("location:", loc)
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//         loc.innerHTML = "Location Tracking not Possible.";
+//     }
+// }
+
+    // function showPosition(position) {
+    //     console.log(position.coords.longitude)
+    //         
+    //     // loc.innerHTML = "longitude:" + position.coords.longitude + " <br> Latitude:" + position.coords.latitude + "<br>";
+    // }
+
